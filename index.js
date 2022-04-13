fe54f6008701d97933f9e64168d4731363a0a8d3
williamjohndias <44379984+williamjohndias@users.noreply.github.com>
2022-04-13 11:00:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf('5336650916:AAGKvE2eiIrcy7EP9ZM4BD9sc0QQsNVdGhw')
+
+const app = express()
+app.get('/', function (req, res) {
+    res.send("Rodando!!")
+})
+
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+// bot.launch()
+bot.startPolling()
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started on port 3000')
+})
