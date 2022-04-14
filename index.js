44a6a36180d89154f79dea89675c140b1d5a3626
williamjohndias <44379984+williamjohndias@users.noreply.github.com>
2022-04-14 11:22:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 
-const { Telegraf } = require('telegraf');
+const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
+
+const session = require('telegraf/session')
 
 const bot = new Telegraf('5336650916:AAGKvE2eiIrcy7EP9ZM4BD9sc0QQsNVdGhw')
 
@@ -13,14 +15,15 @@ app.get('/', function (req, res) {
 })
 
 
-let list = []
 
-const buttons = () => Extra.markup(
+const buttons = list => Extra.markup(
     Markup.inlineKeyboard(
         list.map(item => Markup.callbackButton(item, `delete ${item}`)),
         { columns: 2}
     )
 )
+
+bot.use(session())
     
 
 // bot.launch()
@@ -29,16 +32,20 @@ bot.start( async content => {
     
     await content.reply(`Seja Bem-Vindo(a), ${name}`)
     await content.reply('Digite os produtos que deseja adcionar ao carrinho')
+  content.session.list = []
 })
 
 bot.on('text', content => {
-    list.push(content.update.message.text)
-    content.reply(`${content.update.message.text} Produto adcionado`, buttons() )
+    
+    let msg = content.update.message.text
+  
+    content.session.list.push(msg)
+    content.reply(`${msg} Produto adcionado`, buttons(content.session.list) )
 })
 
 bot.action(/delete (.+)/, content => {
-    list = list.filter(item => item !== content.match[1])   
-    content.reply(`${content.match[1]} deletado`, buttons())
+    content.session.list = content.session.list.filter(item => item !== content.match[1])   
+    content.reply(`${content.match[1]} deletado`, buttons(content.session.list))
 })
 
 bot.startPolling()
